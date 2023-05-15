a9c88174e8e3e4a774c99fbbcb05703b16d3c134
Raymond <raydevries76@gmail.com>
2023-05-15 19:45:29 +0200

    init scical

---
+++ Sci_Cal.py
@@ -0,0 +1,14 @@
+# Scientific Calculator (wetenschappelijke rekenmachine)
+# gebaseerd op: @rahulmallah785671 build-your-own-scientific-calculator-with-python-a-step-by-step-guide
+
+# importeer modules
+import math
+import tkinter as tk
+
+# maak calculator class
+
+    # maak creat button def
+
+    # maak click def
+
+# maak mainloop
